dcd8f146c02f70ae66a5e06bdb21286fb8ffa36e
Scott Obara <scott@rubbscreations.com>
2021-01-19 20:05:11 +1100

    Initial Commit

---
+++ FunWithMath/FunWithMathApp.swift
@@ -0,0 +1,17 @@
+//
+//  FunWithMathApp.swift
+//  FunWithMath
+//
+//  Created by Scott Obara on 19/1/21.
+//
+
+import SwiftUI
+
+@main
+struct FunWithMathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
