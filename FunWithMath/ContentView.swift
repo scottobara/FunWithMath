7c76f2d0b0b9654168d3e2c72f3efbfd013e07a9
Scott Obara <scott@rubbscreations.com>
2021-03-26 13:42:51 +1100

    Unsaved changes

--- FunWithMath/ContentView.swift
+++ FunWithMath/ContentView.swift
@@ -8,8 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var gameActive = false
+    @State private var timesTableSelected = 0
+    
     var body: some View {
-        Text("Hello, world!")
+        
+        VStack {
+            if !gameActive {
+                MenuView()
+            }
+            if gameActive {
+                GameView()
+            }
+            Button ("Toggle") {
+                gameActive.toggle()
+            }
+        }
+        
+    }
+}
+
+struct MenuView: View {
+    
+    let columns = [
+            GridItem(.adaptive(minimum: 80))
+        ]
+    
+    var body: some View {
+        VStack {
+            Text("Chose your times table")
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 100))]) {
+                ForEach(1..<13) {
+                    Button("\($0)"){
+                        //Add stuff here
+                        
+                    }
+                        .font(.title)
+                        .padding()
+                        .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: 50)
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(20)
+                        .shadow(radius: 20)
+                }
+            }
+            .frame(maxWidth: .infinity)
+        }
+    }
+}
+
+
+struct GameView: View {
+    
+    var body: some View {
+        Text("🎰 Game goes here")
             .padding()
     }
 }
